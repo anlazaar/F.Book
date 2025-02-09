621f6ee88529ab89cddc94d93c90d6d1598c7479
Anass Lazaar <anasslazaar9@gmai.com>
2025-02-09 20:46:06 +0100

    test

--- server/Migrations/Migrate.go
+++ server/Migrations/Migrate.go
@@ -1,20 +1,27 @@
 package Migrations
 
 import (
+	"forum/server/GlobVar"
 	"log"
 	"os"
-
-	"forum/server/GlobVar"
 )
 
 func Migrate() {
-	query, err := os.ReadFile("../Database/modules.sql")
+	// Get the file path from an environment variable (fallback to default path)
+	filePath := os.Getenv("MODULES_SQL_PATH")
+	if filePath == "" {
+		filePath = "./server/Database/modules.sql" // Default path if environment variable is not set
+	}
+
+	query, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	_, err = GlobVar.DB.Exec(string(query))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	log.Println("Database migrated successfully!")
 }
